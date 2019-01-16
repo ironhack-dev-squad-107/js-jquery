2c5645e157fb937e642125712b178cf71c69c3ce
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2019-01-16 16:02:33 +0100

    Add game feedback & replay

--- js/script.js
+++ js/script.js
@@ -32,10 +32,71 @@ function selectOpponent() {
   $(".opponent .selection").addClass(chosenClass);
 }
 
+function updateWinnerText() {
+  var isRock = $(".player .selection").hasClass("rock");
+  var isPaper = $(".player .selection").hasClass("paper");
+
+  if (isRock) {
+    // opponent: rock ties, paper we lose, scissors we win
+    playerRock();
+  } else if (isPaper) {
+    // opponent: paper ties, scissors we lose, rock we win
+    playerPaper();
+  } else {
+    // opponent: scissors ties, rock we lose, paper we win
+    playerScissors();
+  }
+
+  // show the game over screen
+  $(".game-end").addClass("showing");
+}
+
+function playerRock() {
+  // opponent: rock ties, paper we lose, scissors we win
+  if ($(".opponent .selection").hasClass("rock")) {
+    $(".game-end h2").html("It's a tie! 😑");
+  } else if ($(".opponent .selection").hasClass("paper")) {
+    $(".game-end h2").html("Opponent wins. 😢");
+  } else {
+    $(".game-end h2").html("You win! 🤩");
+  }
+}
+
+function playerPaper() {
+  // opponent: paper ties, scissors we lose, rock we win
+  if ($(".opponent .selection").hasClass("paper")) {
+    $(".game-end h2").html("It's a tie! 😑");
+  } else if ($(".opponent .selection").hasClass("scissors")) {
+    $(".game-end h2").html("Opponent wins. 😢");
+  } else {
+    $(".game-end h2").html("You win! 🤩");
+  }
+}
+
+function playerScissors() {
+  // opponent: scissors ties, rock we lose, paper we win
+  if ($(".opponent .selection").hasClass("scissors")) {
+    $(".game-end h2").html("It's a tie! 😑");
+  } else if ($(".opponent .selection").hasClass("rock")) {
+    $(".game-end h2").html("Opponent wins. 😢");
+  } else {
+    $(".game-end h2").html("You win! 🤩");
+  }
+}
+
 // WITHOUT jQuery
 // var submit = document.querySelector(".submit-btn");
 // submit.onclick = function () {};
 $(".submit-btn").click(function() {
   // keep your click functions short by using named functions
   selectOpponent();
+  updateWinnerText();
+});
+
+// -----------------------------------------------------------------------------
+
+// "Play Again" button
+$(".game-end button").click(function() {
+  $(".game-end").removeClass("showing");
+  $(".selection").removeClass("rock paper scissors");
 });
